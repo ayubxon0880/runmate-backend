0161a53a0a74ad924582ed109a3561c0ccb55644
Ayubxon Obidov <128991489+ayubxon0880@users.noreply.github.com>
2025-08-20 23:21:42 +0500

    start

---
+++ src/main/java/uz/runmate/RunMate/RunMateApplication.java
@@ -0,0 +1,13 @@
+package uz.runmate.RunMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/runmate/RunMate/RunMateApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.runmate.RunMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
